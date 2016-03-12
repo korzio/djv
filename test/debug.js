7d3baf4155305e572fb58efc6bbef89e83df2c1f
korzio <korzio@gmail.com>
2016-03-12 12:58:30 +0100

    Added umd build version, fixed tests

--- test/debug.js
+++ test/debug.js
@@ -1,38 +1,22 @@
-// var assert = require('assert');
-// var djv = require('../');
-// var utils = require('../lib/utils');
+var assert = require('assert');
+var djv = require('../');
 
-// var testSuite = {
-//     "description":
-//         "additionalProperties allows a schema which should validate",
-//     "schema": {
-//         "properties": {"foo": {}, "bar": {}},
-//         "additionalProperties": {"type": "boolean"}
-//     },
-//     "tests": [
-//         {
-//             "description": "no additional properties is valid",
-//             "data": {"foo": 1},
-//             "valid": true
-//         }
-//     ]
-// };
+var jsonSchema = {
+    "common": {
+        "properties": {
+            "type": {
+                "enum": ["common"]
+            }
+        },
+        "required": [
+            "type"
+        ]
+    }
+};
 
-// var env = new djv();
-// env.addSchema('test', testSuite.schema);
+var env = new djv();
+env.addSchema('test', jsonSchema);
 
-// testSuite.tests.forEach(function (test) {
-//     var errors = env.validate('test', test.data),
-//         status = !errors ? { valid: true } : { valid: false, errors: [errors] };
-
-//     console.log(test.description);
-//     console.log('data', test.data);
-
-//     if (test.valid !== status.valid) {
-//         console.warn('wrong', errors);
-//     } else {
-//         console.info('ok');
-//     }
-// });
-
-// console.info('done');
+var commonObj = { type: 'common' };
+var errors = env.validate('test#/common', commonObj);
+assert.equal(errors, undefined);
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ var webpack = require("webpack");
 
 module.exports = {
     entry: ['./lib/djv.js'],
+    target: 'node',
     output: {
         path: __dirname + '/dist',
+        library: 'djv',
+        libraryTarget: 'umd',
         filename: 'djv.js',
         sourceMapFilename: '[file].map'
     },
