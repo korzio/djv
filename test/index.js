ebd4b37563893a771e9f9a206652f981e6eeb7cb
korzio <korzio@gmail.com>
2016-03-30 14:02:10 +0200

    Added common test, release 0.0.1

--- test/index.js
+++ test/index.js
@@ -14,4 +14,48 @@ describe('djv', function () {
         });
     });
 
+    var jsonSchema = {
+        "common": {
+            "properties": {
+                "type": {
+                    "enum": ["common"]
+                }
+            },
+            "required": [
+                "type"
+            ]
+        }
+    };
+
+    describe('addSchema()', function(){
+        it('should add schema', function () {
+            var env = new djv();
+            env.addSchema('test', jsonSchema);
+            var resolved = env.resolve('test#/common');
+
+            assert.equal(typeof resolved, 'object');
+            assert.equal(typeof resolved.fn, 'function');
+        });
+    });
+
+    describe('validate()', function(){
+        it('should return undefined if object is valid', function () {
+            var env = new djv();
+            env.addSchema('test', jsonSchema);
+
+            var commonObj = { type: 'common' };
+            var errors = env.validate('test#/common', commonObj);
+            assert.equal(errors, undefined);
+        });
+
+        it('should return error if object is not valid', function () {
+            var env = new djv();
+            env.addSchema('test', jsonSchema);
+
+            var commonObj = { type: 'custom' };
+            var errors = env.validate('test#/common', commonObj);
+            assert.equal(typeof errors, 'string');
+        });
+    });
+
 });
