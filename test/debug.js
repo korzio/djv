c98958e345a1a4e6b4f50be8f7109653b6cb1dc9
korzio <korzio@gmail.com>
2016-04-29 16:12:25 +0200

    Moved tests; Changed fn.data to object; Added dependencies, enum

--- test/debug.js
+++ test/debug.js
@@ -1,14 +1,23 @@
 var assert = require('assert');
 var djv = require('../');
 
-var jsonSchema = {
-    "allOf": [
-        {"maximum": 30},
-        {"minimum": 20}
-    ]
+var package = require('./test-suite/draft4/dependencies.json')
+    .find(package => package.description === 'multiple dependencies subschema');
+
+var globalPath = './../node_modules/json-schema-test-suite';
+var refs = {
+    // 'http://localhost:1234/integer.json': require('json-schema-test-suite/remotes/integer.json'),
+    // 'http://localhost:1234/subSchemas.json': require('json-schema-test-suite/remotes/subSchemas.json'),
+    // 'http://localhost:1234/folder/folderInteger.json': require('json-schema-test-suite/remotes/folder/folderInteger.json'),
+    // 'http://json-schema.org/draft-04/schema': require('./resources/draft-04-schema.json')
 };
 
 var env = new djv();
-env.addSchema('test', jsonSchema);
 
-assert.deepEqual(env.instance('test'), 20);
+Object.keys(refs).forEach(function (uri) {
+    env.addSchema(uri, refs[uri]);
+});
+
+env.addSchema('test', package.schema);
+
+// assert.deepEqual(env.instance('test'), package.tests[0]);
--- test/tests-suite.js
+++ test/tests-suite.js
@@ -3,8 +3,8 @@ var djv = require('../');
 var fs = require('fs');
 
 describe('Local tests suite', function () {
-    fs.readdirSync('test/suite').forEach(file => {
-        var packages = require('./suite/' + file);
+    fs.readdirSync('test/data').forEach(file => {
+        var packages = require('./data/' + file);
         describe(file, () => {
             packages.forEach(package => {
                 package.tests.forEach(test => {
@@ -21,24 +21,45 @@ describe('Local tests suite', function () {
     })
 })
 
+
+var globalPath = './../node_modules/json-schema-test-suite';
+var refs = {
+    'http://localhost:1234/integer.json': require('json-schema-test-suite/remotes/integer.json'),
+    'http://localhost:1234/subSchemas.json': require('json-schema-test-suite/remotes/subSchemas.json'),
+    'http://localhost:1234/folder/folderInteger.json': require('json-schema-test-suite/remotes/folder/folderInteger.json'),
+    'http://json-schema.org/draft-04/schema': require('./resources/draft-04-schema.json')
+};
+
 describe('General tests suite', function () {
     // fs.readdirSync('test/draft4').forEach(file => {
     [
         'additionalItems',
         'additionalProperties',
-        'items',
         'allOf',
-        'anyOf'
-    ].forEach(file => {
-        var packages = require('./draft4/' + file);
-        packages.forEach(package => {
-            package.tests.forEach(test => {
-                it(package.description + ' - ' + test.description, function () {
-                    var env = new djv();
-                    env.addSchema('test', package.schema);
-                    var instance = env.instance('test');
+        'anyOf',
+        'default',
+        'definitions',
+        'dependencies',
+        'enum',
 
-                    assert.deepEqual(instance, test.data);
+        'items'
+    ].forEach(file => {
+        var packages = require('./test-suite/draft4/' + file);
+        describe(file, () => {
+            packages.forEach(package => {
+                package.tests.forEach(test => {
+                    it(package.description + ' - ' + test.description, function () {
+                        var env = new djv();
+
+                        Object.keys(refs).forEach(function (uri) {
+                            env.addSchema(uri, refs[uri]);
+                        });
+
+                        env.addSchema('test', package.schema);
+                        var instance = env.instance('test');
+
+                        assert.deepEqual(instance, test.data);
+                    })
                 })
             })
         })
